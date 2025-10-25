a06eaeef642d62522b676b62381fecfd34a0817c
Markus357 <mdavidson357@gmail.com>
2025-10-25 14:27:51 -0400

    feat: add i18next configuration with EN/FR translations

---
+++ src/i18n/config.ts
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './en.json';
+import fr from './fr.json';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  fr: {
+    translation: fr,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    
+    interpolation: {
+      escapeValue: false,
+    },
+    
+    defaultNS: 'translation',
+    ns: ['translation'],
+  });
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GlobalStyle } from './styles/global'
+import './i18n/config'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { t } = useTranslation()
   return (
     <>
       <GlobalStyle />
-      <h1 style={{ color: 'var(--royal-blue)' }}>Nesto Mortgage Application</h1>
+      <h1 style={{ color: 'var(--royal-blue)' }}>
+        {t('title')}
+      </h1>
     </>
   );
 }
