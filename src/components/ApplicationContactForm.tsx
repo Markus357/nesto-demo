0550c69ec5f96876811cdf40d9c46df5e549b2c7
Markus357 <mdavidson357@gmail.com>
2025-10-28 22:57:11 -0400

    feat: update ApplicationContactForm - make sure initialData is properly populated, responsive button, loading button text

--- src/components/ApplicationContactForm.tsx
+++ src/components/ApplicationContactForm.tsx
@@ -1,46 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { InputField, InputFieldWrapper } from './InputField';
-import { Button } from './Button';
+import { Button, StyledButton } from './Button';
 import type { ContactFormData } from '../types';
 
 interface ApplicationContactFormProps {
   onSubmit: (data: ContactFormData) => void;
   initialData?: Partial<ContactFormData>;
   isLoading?: boolean;
+  loadingButtonText?: string;
 }
 
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
+  width: 100%;
 
   ${InputFieldWrapper}:not(:last-child) {
     margin-bottom: 16px;
   }
 `;
 
+
 const ButtonContainer = styled.div`
   margin-top: 16px;
-  display: flex;
-  justify-content: flex-end;
+
+  @media (min-width: 900px) {
+    ${StyledButton} {
+      width: auto;
+    }
+  }
 `;
 
 export const ApplicationContactForm: React.FC<ApplicationContactFormProps> = ({
   onSubmit,
   initialData,
   isLoading = false,
+  loadingButtonText,
 }) => {
   const { t } = useTranslation();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<ContactFormData>({
     defaultValues: initialData,
     mode: 'onChange',
   });
+
+  useEffect(() => {
+    if (initialData) {
+      reset(initialData as ContactFormData);
+    }
+  }, [initialData, reset]);
 
   return (
     <FormContainer onSubmit={handleSubmit(onSubmit)} noValidate>
@@ -107,8 +122,10 @@ export const ApplicationContactForm: React.FC<ApplicationContactFormProps> = ({
       />
 
       <ButtonContainer>
-        <Button type="submit" disabled={isLoading}>
-          {isLoading ? t('form.submitLoading') : t('form.submit')}
+        <Button type="submit" disabled={isLoading} fullWidth>
+          {isLoading
+            ? (loadingButtonText ?? t('form.submitLoading'))
+            : (loadingButtonText ?? t('form.submit'))}
         </Button>
       </ButtonContainer>
     </FormContainer>
