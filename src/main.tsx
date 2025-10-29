3c98f1b293fbb663250b331f8351cc389a592fb7
Markus357 <mdavidson357@gmail.com>
2025-10-29 10:44:53 -0400

    feat: update favicon

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen'
 import './i18n/config'
+import mobileFavicon from './assets/nestoIcon-Primary-sm.png'
 
 const router = createRouter({ routeTree })
 
@@ -22,3 +23,9 @@ createRoot(document.getElementById('root')!).render(
     </QueryClientProvider>
   </StrictMode>,
 )
+
+// Set favicon to mobile logo
+const faviconEl = document.querySelector<HTMLLinkElement>("link[rel='icon']")
+if (faviconEl) {
+  faviconEl.href = mobileFavicon
+}
